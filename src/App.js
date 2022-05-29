c5b383289705b2c76af990eed3a0e02e35b8132f
up1512001 <utsav.patel1215@outlook.com>
2022-05-29 18:51:56 +0530

    UI part for showing list of coins DONE

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React , { useState , useEffect } from "react";
 import axios from "axios";
 import Coins from "./components/Coins";
 
+import Navbar from './components/Navbar'
+
 function App() {
 
   const  [coins,setCoins] = useState([])
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
 
   useEffect(() =>{
     axios.get(url).then((response) =>{
@@ -21,6 +23,7 @@ function App() {
 
   return (
     < >
+    <Navbar/>
     <Coins coins={coins}/>
 
     </>
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import CoinItem from './CoinItem'
+
+import './Coins.css'
 
 const Coins = (props) => {
   return (
@@ -10,9 +13,15 @@ const Coins = (props) => {
                 <p>Price</p>
                 <p>24h</p>
                 <p className='hide-mobile'>Volume</p>
-                <p className='hide-mobile'>Mkt Cap</p>
-                    
+                <p className='hide-mobile'>Mkt Cap</p>                    
             </div>
+
+            {props.coins.map(coins => {
+              return (
+                <CoinItem coins = {coins} key={coins.id}  />
+              )
+            })}
+
         </div>
       </div>
   )
