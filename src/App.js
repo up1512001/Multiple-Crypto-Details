ab84c937cfc40577d452b7ba1f3d3ded55d8ac4b
up1512001 <utsav.patel1215@outlook.com>
2022-06-16 00:14:51 +0530

    required updates are made

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import React , { useState , useEffect } from "react";
-
-import axios from "axios";
-import Coins from "./components/Coins";
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+import Coins from './components/Coins'
+import Coin from './routes/Coin'
 import Navbar from './components/Navbar'
 
-import {Routes , Route} from 'react-router-dom'
-
-import Coin from "./routes/Coin";
-
-import './index.css'
 
 function App() {
 
-  const  [coins,setCoins] = useState([])
+  const [coins, setCoins] = useState([])
 
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
 
-  useEffect(() =>{
-    axios.get(url).then((response) =>{
+  useEffect(() => {
+    axios.get(url).then((response) => {
       setCoins(response.data)
-      console.log(response.data[0])
-    }).catch((error)=>{
-      // console.log(error)
+      // console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
     })
-  },[])
-
+  }, [])
 
   return (
-    < >
-    <Navbar/>
-    <Routes>
+    <>
+      <Navbar />
+      <Routes>
         <Route path='/' element={<Coins coins={coins} />} />
-        <Route path='/coin' element = {<Coin/>}>
-          <Route  path=':coinId' element={<Coin/>} />
-        </Route> 
-    </Routes>
-    
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
+        </Route>
+      </Routes>
 
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,15 @@
 import React from 'react'
-
-import {FaCoins} from 'react-icons/fa'
-
+import { FaCoins } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
-
-import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <Link to='/'>
-        <div className='navbar'>
-            <FaCoins className='icon'/>
-            <h1> Coin <span className='purple'>Search</span></h1>
-        </div>
+      <div className='navbar'>
+        <FaCoins className='icon' />
+        <h1> Coin <span className='purple'>Search</span></h1>
+      </div>
     </Link>
   )
 }
